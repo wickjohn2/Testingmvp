85db49c0162cd6dd68fcd03fb35f7c879ac5391a
wickjohn2 <johnwick2@gmail.com>
2017-02-28 17:31:54 -0600

    Update Environment_MVP.py

--- Environment_MVP.py
+++ Environment_MVP.py
@@ -1,17 +1,12 @@
 """Python template for creating a pool of similar instances."""
 
 def GenerateConfig(context):
-
-  machine_type = ''.join(['https://www.googleapis.com/compute/v1/projects/',
-                          context.env['project'], '/zones/',
-                          context.properties['zone'], '/machineTypes/',
-                          context.properties['machineType']])
   
   resources = {
       'type': 'compute.v1.instanceGroup.managed',
       'name': context.properties['name'],
       'properties': {
-          'size': resources
+          'size': context.properties['count'],
           'template': context.properties['template']
           'zone': context.properties['zone'],
           'base-instance-name': adlib
