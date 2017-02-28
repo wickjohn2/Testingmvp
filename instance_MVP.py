6489f7228a56007525ac1cda97dac586c04b8751
wickjohn2 <johnwick2@gmail.com>
2017-02-28 16:56:42 -0600

    Update instance_MVP.py

--- instance_MVP.py
+++ instance_MVP.py
@@ -1,13 +1,3 @@
-"""Python template for creating a pool of similar instances."""
-
-def GenerateConfig(context):
-  """Generate the yaml config for a pool of instances."""
-  resources = []
-  for index in range(1, context.properties['count'] + 1):
-    resources.append(GenerateInstanceConfig(context, index))
-  return {'resources': resources}
-
-
 def GenerateInstanceConfig(context, index):
   """Helper method to create the config for a single compute instance."""
   name_prefix = ''.join([context.properties['namePrefix'], '-',
