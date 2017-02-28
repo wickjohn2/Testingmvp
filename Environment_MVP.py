80f690de51048d6df1d0c56adac411511c001179
wickjohn2 <johnwick2@gmail.com>
2017-02-28 17:26:14 -0600

    Update Environment_MVP.py

--- Environment_MVP.py
+++ Environment_MVP.py
@@ -1,25 +1,15 @@
 """Python template for creating a pool of similar instances."""
 
 def GenerateConfig(context):
-  """Generate the yaml config for a pool of instances."""
-  resources = []
-  for index in range(1, context.properties['count'] + 1):
-    resources.append(GenerateInstanceConfig(context, index))
-  return {'resources': resources}
 
-
-def GenerateInstanceConfig(context, index):
-  """Helper method to create the config for a single compute instance."""
-  name_prefix = ''.join([context.properties['namePrefix'], '-',
-                         context.env['deployment'], '-', str(index)])
   machine_type = ''.join(['https://www.googleapis.com/compute/v1/projects/',
                           context.env['project'], '/zones/',
                           context.properties['zone'], '/machineTypes/',
                           context.properties['machineType']])
   
-  instance_group = {
+  resources = {
       'type': 'compute.v1.instanceGroup.managed',
-      'name': name_prefix,
+      'name': context.properties['name'],
       'properties': {
           'size': resources
           'template': context.properties['template']
@@ -27,4 +17,4 @@ def GenerateInstanceConfig(context, index):
           'base-instance-name': adlib
       }
   }
-  return instance_group
+  return {'resources': resources}
